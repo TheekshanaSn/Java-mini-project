a3e9dee98dfb5a576ea528b57b4230dabdb32772
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2025-04-05 12:20:37 +0530

    Update Attendance.java
    change code

--- To_AND_Attandence/src/Attendance.java
+++ To_AND_Attandence/src/Attendance.java
@@ -1,9 +1,8 @@
 public class Attendance {
 
     public  Attendance() {
-
+        System.out.println("Attendance");
     }
-
     public static void main(String[] args) {
         System.out.print("hellow world");
     }
