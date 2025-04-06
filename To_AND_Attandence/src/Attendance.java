02dbc9166b04bd861e2b19aafe4322dbf86bff45
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2025-04-06 19:08:53 +0530

    Update Attendance.java

--- To_AND_Attandence/src/Attendance.java
+++ To_AND_Attandence/src/Attendance.java
@@ -13,5 +13,6 @@ public class Attendance {
         System.out.println("wjbb");
         System.out.println("tuyttgtgt");
         System.out.println(a.getClass());
+        System.out.println();
     }
 }
