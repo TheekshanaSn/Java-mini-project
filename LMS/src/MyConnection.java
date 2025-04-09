f76c27bdabc08229e1a30e47acf19c17731eef27
TheekshanaSn <theekshanasankalapa2002@gmail.com>
2025-04-09 20:54:08 +0530

    Update MyConnection.java
    update Connection

--- LMS/src/MyConnection.java
+++ LMS/src/MyConnection.java
@@ -1,25 +1,21 @@
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class MyConnection {
 
 
     public static void main(String[] args) {
 
-         String url = "jdbc:mysql://localhost:3306/apollo";
-         String username = "root";// my roots
-         String password = "";
-
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/apollo","root","");
-            Statement stmt = ((java.sql.Connection) con).createStatement();
+     Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/apollo","root","");
+            Statement stmt = con.createStatement();
 
-            ResultSet rs = stmt.executeQuery("select * from user");
+           ResultSet rs = stmt.executeQuery("select * from user");
             while(rs.next()){
                 System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4)+" "+rs.getString(5)+" "+rs.getString(6));
             }
 
-//            ((java.sql.Connection) con).close();
+      con.close();
 
         }catch (Exception e){
             System.out.println(e.getMessage());
