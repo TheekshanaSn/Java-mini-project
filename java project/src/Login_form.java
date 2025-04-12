caacac46ad263f39ae6d90341b1aa3458d9eb520
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2025-04-12 16:19:50 +0530

    login form gui

---
+++ java project/src/Login_form.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Login_form {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton loginButton;
+    private JButton cancelButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
