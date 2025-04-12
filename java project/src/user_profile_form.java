923545850a0596124890373e37bdfa0a5ea17245
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2025-04-12 16:20:20 +0530

    use profile

---
+++ java project/src/user_profile_form.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class user_profile_form {
+    private JButton userButton;
+    private JButton courseButton;
+    private JButton noticeButton;
+    private JButton timetableButton;
+    private JPanel JPanel1;
+    private JPanel JPanelMain;
+    private JPanel JPanel2;
+    private JButton undegraduateButton;
+    private JButton lecturerButton;
+    private JButton technicalOfficerButton;
+    private JButton addNewButton;
+    private JButton singOutButton;
+    private JTable table1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+
+    }
+}
