fb2c256fdec6763ead6c2cedbbd668a52f402cc0
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2025-04-13 21:47:22 +0530

    update

--- java project/src/Notice.java
+++ java project/src/Notice.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class Notice {
+    private JButton userButton;
+    private JButton courseButton;
+    private JButton noticeButton;
+    private JButton timetableButton;
+    private JButton singOutButton;
+    private JButton updateButton;
+    private JButton deleteButton;
+    private JButton updateButton1;
+    private JButton deleteButton1;
+    private JButton updateButton2;
+    private JButton deleteButton2;
+    private JButton addNewButton;
 }
