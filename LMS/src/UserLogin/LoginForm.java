da24c19cc07833ae00033b873685a25db44d4bd8
Thishan Herath <diniduthishan9@gmail.com>
2025-04-18 04:57:40 +0530

    User login phase 1

---
+++ LMS/src/UserLogin/LoginForm.java
@@ -0,0 +1,11 @@
+package UserLogin;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JComboBox comboBox1;
+    private JButton loginButton;
+    private JButton cancelButton;
+}
