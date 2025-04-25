ff8013e546e17151d7b3d1e139b2c8a887556352
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2025-04-25 17:28:12 +0530

    changes

--- LMS/src/ADMIN/DatabaseConnect.java
+++ LMS/src/ADMIN/DatabaseConnect.java
@@ -13,13 +13,17 @@ public class DatabaseConnect {
     private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
 //        try {
 //            System.out.println("succesfully connected to database");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+//            return DriverManager.getConnection(URL, USER, PASSWORD);
 //        } catch (SQLException e) {
 //            throw new RuntimeException("Database connection failed: " + e.getMessage(), e);
 //        }
-    }
-}
+//    }
+//}
 
 
