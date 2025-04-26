b1c902573ade2722b7fbb457ff07120ca9faf0d1
Krishani-imalka <159682806+Krishani-imalka@users.noreply.github.com>
2025-04-26 23:28:42 +0530

    commited

--- LMS/src/ADMIN/DatabaseConnect.java
+++ LMS/src/ADMIN/DatabaseConnect.java
@@ -1,21 +1,21 @@
-package ADMIN;
-
-import java.sql.*;
+//package ADMIN;
+//
+//import java.sql.*;
 //import java.sql.Connection;
 //import java.sql.DriverManager;
 //import java.sql.SQLException;
-
-
-public class DatabaseConnect {
-    private static final String URL = "jdbc:mysql://localhost:3306/techlmsk";
+//
+//
+//public class DatabaseConnect {
+//    private static final String URL = "jdbc:mysql://localhost:3306/techlmsk";
 //   private static final String URL = "jdbc:mysql://127.0.0.1:3306/techlmsk";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-}
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "";
+//
+//    public static Connection getConnection() throws SQLException {
+//        return DriverManager.getConnection(URL, USER, PASSWORD);
+//    }
+//}
 
 //        try {
 //            System.out.println("succesfully connected to database");
